307a0715d69c1fe6685071da8ccedbb380d0bc1b
Sukanta Som <skntsom@gmail.com>
2018-12-03 20:03:23 +0530

    final codes
    Code with GUI

--- song_recommendation.py
+++ song_recommendation.py
@@ -1,14 +1,13 @@
-#%matplotlib inline
+
 
 import pandas
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection  import train_test_split
 import numpy as np
 import time
 from sklearn.externals import joblib
 import Recommenders as Recommenders
 import Evaluation as Evaluation
 
-#Read userid-songid-listen_count triplets
 #This step might take time to download data from external sources
 triplets_file = '10000.txt'
 songs_metadata_file = 'song_data.csv'
@@ -22,113 +21,41 @@ song_df_2 =  pandas.read_csv(songs_metadata_file)
 #Merge the two dataframes above to create input dataframe for recommender systems
 song_df = pandas.merge(song_df_1, song_df_2.drop_duplicates(['song_id']), on="song_id", how="left")
 
-#EXPLORE DATA
-song_df.head()
-
-#LENGTH OF DATASET
-len(song_df)
-
 #CREATE SUBSETOF DAATASET
 song_df=song_df.head(10000)
 #Merge song title and artist_name columns to make a merged column
 song_df['song'] = song_df['title'].map(str) + " - " + song_df['artist_name']
 
-#SHOWING THE MOST POPULAR SONG IN DATASET
-song_grouped = song_df.groupby(['song']).agg({'listen_count': 'count'}).reset_index()
-grouped_sum = song_grouped['listen_count'].sum()
-song_grouped['percentage']  = song_grouped['listen_count'].div(grouped_sum)*100
-song_grouped.sort_values(['listen_count', 'song'], ascending = [0,1])
-
-#Count number of unique users in the dataset
-users = song_df['user_id'].unique()
-len(users)
-#COUNT NUMBER OF UNIQUE SONGS IN THE DATASET
-songs=song_df['song'].unique()
-len(songs)
-
-
 #CREATE A SONG RECOMMENDER
 train_data,test_data=train_test_split(song_df,test_size=0.20,random_state=0)
-print(train_data.head())
+#print(train_data.head(5))
 
-#Recommenders.popularity_recommender_py
+#============================popularity based recommendation============================
+
 pm = Recommenders.popularity_recommender_py()
 pm.create(train_data, 'user_id', 'song')
 
-user_id = users[5]
-pm.recommend(user_id)
 
-###Fill in the code here
-user_id = users[8]
-pm.recommend(user_id)
-
-#Recommenders.item_similarity_recommender_py
+#============================item_similarity based recommendation=================
 is_model = Recommenders.item_similarity_recommender_py()
 is_model.create(train_data, 'user_id', 'song')
-
-#Print the songs for the user in training data
-user_id = users[5]
-user_items = is_model.get_user_items(user_id)
-#
-print("------------------------------------------------------------------------------------")
-print("Training data songs for the user userid: %s:" % user_id)
-print("------------------------------------------------------------------------------------")
-
-for user_item in user_items:
-    print(user_item)
-
-print("----------------------------------------------------------------------")
-print("Recommendation process going on:")
-print("----------------------------------------------------------------------")
-
-#Recommend songs for the user using personalized model
-is_model.recommend(user_id)
-
-user_id = users[7]
-#Fill in the code here
-user_items = is_model.get_user_items(user_id)
-#
-print("------------------------------------------------------------------------------------")
-print("Training data songs for the user userid: %s:" % user_id)
-print("------------------------------------------------------------------------------------")
-
-for user_item in user_items:
-    print(user_item)
-
-print("----------------------------------------------------------------------")
-print("Recommendation process going on:")
-print("----------------------------------------------------------------------")
-
-#Recommend songs for the user using personalized model
-is_model.recommend(user_id)
-
-is_model.get_similar_items(['U Smile - Justin Bieber'])
-
-song = 'Yellow - Coldplay'
-###Fill in the code here
-is_model.get_similar_items([song])
-
-#Evaluation.precision_recall_calculator
-start = time.time()
-
+#==============================plot graph=============================
 #Define what percentage of users to use for precision recall calculation
 user_sample = 0.05
 
-#Instantiate the precision_recall_calculator class
+#Instantiate the precision_recall_calculator class <pm-popularity based recc. model>,<is_model-Item similarity based>
 pr = Evaluation.precision_recall_calculator(test_data, train_data, pm, is_model)
 
 #Call method to calculate precision and recall values
 (pm_avg_precision_list, pm_avg_recall_list, ism_avg_precision_list, ism_avg_recall_list) = pr.calculate_measures(user_sample)
 
-end = time.time()
-print(end - start)
 
 #Code to plot precision recall curve
 import pylab as pl
 
 #Method to generate precision and recall curve
 def plot_precision_recall(m1_precision_list, m1_recall_list, m1_label, m2_precision_list, m2_recall_list, m2_label):
-    pl.clf()    
+    pl.clf()
     pl.plot(m1_recall_list, m1_precision_list, label=m1_label)
     pl.plot(m2_recall_list, m2_precision_list, label=m2_label)
     pl.xlabel('Recall')
@@ -139,140 +66,197 @@ def plot_precision_recall(m1_precision_list, m1_recall_list, m1_label, m2_precis
     #pl.legend(loc="upper right")
     pl.legend(loc=9, bbox_to_anchor=(0.5, -0.2))
     pl.show()
-print("Plotting precision recall curves.")
 
-plot_precision_recall(pm_avg_precision_list, pm_avg_recall_list, "popularity_model",
-                      ism_avg_precision_list, ism_avg_recall_list, "item_similarity_model")
+#xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
-#Generate Precision Recall curve using pickled results on a larger data subset(Python 3)
+from tkinter import *
+from PIL import ImageTk,Image
+panel = Tk()
+panel.title("musik")
 
-print("Plotting precision recall curves for a larger subset of data (100,000 rows) (user sample = 0.005).")
-
-#Read the persisted files 
-pm_avg_precision_list = joblib.load('pm_avg_precision_list_3.pkl')
-pm_avg_recall_list = joblib.load('pm_avg_recall_list_3.pkl')
-ism_avg_precision_list = joblib.load('ism_avg_precision_list_3.pkl')
-ism_avg_recall_list = joblib.load('ism_avg_recall_list_3.pkl')
-
-print("Plotting precision recall curves.")
-plot_precision_recall(pm_avg_precision_list, pm_avg_recall_list, "popularity_model",
-                      ism_avg_precision_list, ism_avg_recall_list, "item_similarity_model")
-
-#Generate Precision Recall curve using pickled results on a larger data subset(Python 2.7)
-print("Plotting precision recall curves for a larger subset of data (100,000 rows) (user sample = 0.005).")
-
-pm_avg_precision_list = joblib.load('pm_avg_precision_list_2.pkl')
-pm_avg_recall_list = joblib.load('pm_avg_recall_list_2.pkl')
-ism_avg_precision_list = joblib.load('ism_avg_precision_list_2.pkl')
-ism_avg_recall_list = joblib.load('ism_avg_recall_list_2.pkl')
-
-print("Plotting precision recall curves.")
-plot_precision_recall(pm_avg_precision_list, pm_avg_recall_list, "popularity_model",
-                      ism_avg_precision_list, ism_avg_recall_list, "item_similarity_model")
-
-#Matrix Factorization based Recommender System
-#Code source written with help from: 
-#http://antoinevastel.github.io/machine%20learning/python/2016/02/14/svd-recommender-system.html
-
-import math as mt
-import csv
-from sparsesvd import sparsesvd #used for matrix factorization
-import numpy as np
-from scipy.sparse import csc_matrix #used for sparse matrix
-from scipy.sparse.linalg import * #used for matrix multiplication
-
-#Note: You may need to install the library sparsesvd. Documentation for 
-#sparsesvd method can be found here:
-#https://pypi.python.org/pypi/sparsesvd/
-
-#constants defining the dimensions of our User Rating Matrix (URM)
-MAX_PID = 4
-MAX_UID = 5
-
-#Compute SVD of the user ratings matrix
-def computeSVD(urm, K):
-    U, s, Vt = sparsesvd(urm, K)
-
-    dim = (len(s), len(s))
-    S = np.zeros(dim, dtype=np.float32)
-    for i in range(0, len(s)):
-        S[i,i] = mt.sqrt(s[i])
-
-    U = csc_matrix(np.transpose(U), dtype=np.float32)
-    S = csc_matrix(S, dtype=np.float32)
-    Vt = csc_matrix(Vt, dtype=np.float32)
-    
-    return U, S, Vt
-
-#Compute estimated rating for the test user
-def computeEstimatedRatings(urm, U, S, Vt, uTest, K, test):
-    rightTerm = S*Vt 
-
-    estimatedRatings = np.zeros(shape=(MAX_UID, MAX_PID), dtype=np.float16)
-    for userTest in uTest:
-        prod = U[userTest, :]*rightTerm
-        #we convert the vector to dense format in order to get the indices 
-        #of the movies with the best estimated ratings 
-        estimatedRatings[userTest, :] = prod.todense()
-        recom = (-estimatedRatings[userTest, :]).argsort()[:250]
-    return recom
-
-#Use SVD to make predictions for a test user id, say 4
-#Used in SVD calculation (number of latent factors)
-K=2
-
-#Initialize a sample user rating matrix
-urm = np.array([[3, 1, 2, 3],[4, 3, 4, 3],[3, 2, 1, 5], [1, 6, 5, 2], [5, 0,0 , 0]])
-urm = csc_matrix(urm, dtype=np.float32)
-
-#Compute SVD of the input user ratings matrix
-U, S, Vt = computeSVD(urm, K)
-
-#Test user set as user_id 4 with ratings [0, 0, 5, 0]
-uTest = [4]
-print("User id for whom recommendations are needed: %d" % uTest[0])
-
-#Get estimated rating for test user
-print("Predictied ratings:")
-uTest_recommended_items = computeEstimatedRatings(urm, U, S, Vt, uTest, K, True)
-print(uTest_recommended_items)
+#===============================IMAGES==================================
+img=ImageTk.PhotoImage(Image.open('pic1.jpg'))
+root=Label(panel,image=img,width=1600,height=800)
+root.pack(side=TOP,fill=BOTH,expand=YES)
 
 
 
-#%matplotlib inline
-from pylab import *
+frame = Frame(root)
+frame.pack()
 
-#Plot all the users
-print("Matrix Dimensions for U")
-print(U.shape)
+Id= StringVar()
+lis=StringVar()
+art=StringVar()
+def plot_data():
+    plot_precision_recall(pm_avg_precision_list, pm_avg_recall_list, "popularity_model",ism_avg_precision_list, ism_avg_recall_list, "item_similarity_model")
 
-for i in range(0, U.shape[0]):
-    plot(U[i,0], U[i,1], marker = "*", label="user"+str(i))
+def search_ar():
+    sgsl = art.get()
+    root3 = Tk()
+    print(sgsl)
+    root3.title("Musik")
+    L1 = Label(root3, text="Artist greatest hits", font=(" Comic Sans MS", 15, "bold"), relief="sunken")
+    L1.pack()
 
-for j in range(0, Vt.T.shape[0]):
-    plot(Vt.T[j,0], Vt.T[j,1], marker = 'd', label="item"+str(j))    
-    
-legend(loc="upper right")
-title('User vectors in the Latent semantic space')
-ylim([-0.7, 0.7])
-xlim([-0.7, 0])
-show()
+    # Show output of similer song list
 
+    Lst4 = Listbox(root3, width=60, bg='gold', selectforeground='cyan')
+    Lst4.pack(side=RIGHT, expand=0.5, fill=Y)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+    arts=song_df[song_df['artist_name']== sgsl]
+    #POPULAR ARTIST
+    song_artist = arts.groupby(['song']).agg({'listen_count': 'count'}).reset_index()
+    similar=song_artist.sort_values(['listen_count'],ascending =False)
+    #similar=similar['song']+"  -  " + similar['listen_count'].map(str)
+    for q in similar['song']:
+        Lst4.insert(END, q)
+    root3.configure(bg='skyblue')
+    root3.mainloop()
 
 
 
+def search_s():
+    sgsl = lis.get()
+    root3 = Tk()
+    root3.title("Musik")
+    L1 = Label(root3, text="Similar Musics are here", font=(" Comic Sans MS", 15, "bold"), relief="sunken")
+    L1.pack()
 
+    # Show output of similer song list
+
+    Lst4 = Listbox(root3, width=60, bg='gold', selectforeground='cyan')
+    Lst4.pack(side=RIGHT, expand=0.5, fill=Y)
+    sim=is_model.get_similar_items(sgsl)
+    sim=sim['rank'].map(str)+"  -  " + sim['song']
+    Lst4.insert(1,' '+sgsl)
+    ct=2
+    for x in sim:
+        Lst4.insert(ct,x)
+        ct+=1
+
+    root3.configure(bg='skyblue')
+    root3.mainloop()
+
+
+def person():
+
+
+    entry=Id.get()
+
+    #Got The User Id in "entry" Perform ML
+
+    #Show output From here
+    #print(entry)
+    root2 = Tk()
+    root2.title("musik")
+
+    b_frame = Frame(root2,bg="skyblue")
+    b_frame.pack()
+
+    #show User Id At Top
+    L1 = Label(b_frame, text=" Result For User:-<<"+entry+">>", font=(" Comic Sans MS", 15, "bold", "underline"), relief="sunken")
+    L1.pack()
+
+    L2 = Label(b_frame, text="SONG playing History", font=(" Comic Sans MS", 15, "bold"), relief="sunken",width=40)
+    L2.pack(side=LEFT,expand=1)
+
+    L3 = Label(b_frame, text="Only for you", font=(" Comic Sans MS", 15, "bold"), relief="sunken",width=40)
+    L3.pack(side=RIGHT,expand=1)
+
+    L4 = Label(b_frame, text="Popular Songs", font=(" Comic Sans MS", 15, "bold"), relief="sunken", width=40)
+    L4.pack(side=RIGHT, expand=1)
+
+    #Output For perticuler User
+    #There'll be 3 lists under "root2"
+    #"Only For You","Populer Songs","played Songs"
+    #enter items for PLAYING HISTORY
+    Lst = Listbox(root2,width=60,bg='gold',selectforeground='cyan')
+    Lst.pack(side=LEFT, fill=Y,expand=0.5)
+    user_items = is_model.get_user_items(entry)
+    cnt=1
+    for x in user_items:
+        Lst.insert(cnt,x)
+        cnt+=1
+
+
+    #enter items for ONLY FOR YOU
+    Lst1 = Listbox(root2,width=60,bg='gold',selectforeground='cyan')
+    Lst1.pack(side=RIGHT,expand=0.5, fill=Y)
+    out=is_model.recommend(entry)
+    out=out['rank'].map(str)+" - "+out['score'].map(str) + " - " + out['song']
+    cnt=1
+    for x in out:
+        Lst1.insert(cnt,x)
+        cnt+=1
+
+    #enter items for POPULAR SONGS
+    Lst2 = Listbox(root2,width=60,bg='gold',selectforeground='cyan')
+    Lst2.pack(side=LEFT,fill=Y,expand=0.5)
+    output=pm.recommend(entry)
+    output=output['Rank'].map(str)+" - "+output['score'].map(str)+" - "+output['song']
+    count=1
+    for x in output:
+        Lst2.insert(count,x)
+        count+=1
+
+
+
+
+    root2.geometry("1300x660+120+120")
+    root2.configure(bg='skyblue')
+    root2.mainloop()
+
+
+
+Toolbar=Frame(root,bg="yellow green",relief="sunken")
+
+L1 = Label(Toolbar, text="Search Songs", font=(" Comic Sans MS", 15, "italic"),fg='red', relief="sunken")
+L1.pack(side=LEFT)
+
+entry_2 = Entry(Toolbar, textvariable= lis, bd=4, font=("Times", 15, "bold"), bg='beige', fg='Brown')
+entry_2.pack(side=LEFT)
+
+button1 = Button(Toolbar, text="Search", font=("Times", 12, "bold"), bg='Blue',command=search_s)
+button1.pack(side=LEFT)
+
+entry_2 = Entry(Toolbar, textvariable=art, bd=4, font=("Times", 15, "bold"), bg='beige', fg='Brown')
+entry_2.pack(side=LEFT)
+
+button3 = Button(Toolbar, text="Artist Hits", font=("Times", 12, "bold"), bg='Blue',command=search_ar)
+button3.pack(side=LEFT)
+
+button = Button(Toolbar, text="Dig In",  font=("Times", 12, "bold"), bg= 'Blue' , command= person,padx=4,pady=4 )
+button.pack(side=RIGHT)
+
+entry_1=Entry(Toolbar,textvariable= Id, bd=5, font= ("Times", 15, "bold"),bg='beige',fg='Brown')
+entry_1.pack(side=RIGHT)
+
+L3 = Label(Toolbar, text="Enter UserID", font=(" Comic Sans MS", 15, "italic"),fg='red', relief="sunken",padx=4,pady=4)
+L3.pack(side=RIGHT)
+
+button2 = Button(Toolbar, text="Plot", font=("Times", 12, "bold"), bg='Blue',padx=2,pady=2,command=plot_data)
+button2.pack()
+
+Toolbar.pack(side=TOP,fill=X)
+
+frame = Frame(root)
+frame.pack()
+
+Lb2 = Label(frame, text="All Songs", font=(" Comic Sans MS", 15, "bold"), relief="sunken", width=50)
+Lb2.pack(expand=1)
+
+
+#output of First Page
+#All Songs and Hit Songs
+List1= Listbox(root,width=100,bg='gold',selectforeground='cyan')
+songlist=song_df['song']
+for q in songlist:
+    List1.insert(END,q)
+
+List1.pack(side= LEFT,expand=0.5,fill=Y)
+
+
+panel.geometry("1300x660+120+120")
+panel.configure(bg='skyblue')
+frame.configure(bg='#26261f')
+panel.mainloop()
